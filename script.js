04adc51f09c0f74770957e84a65c93ffe29f5615
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-06-26 20:04:40 -0300

    2606

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultados = document.querySelector('.caixa-resultados');
 const textoResultado = document.querySelector('.texto-resultado');
-const lista = [item1, item2]
+
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
