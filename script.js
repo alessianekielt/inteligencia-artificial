0d879f96f304f4d42cea113cff02bf62eebe7ad2
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-06-26 19:57:59 -0300

    2606

--- script.js
+++ script.js
@@ -7,27 +7,32 @@ const lista = [item1, item2]
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
-        alternativa: ["Isso é assustador!", 
+        alternativas: [
+            "Isso é assustador!", 
             "Isso é maravilhoso!"],
     },
     {
         enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
-        alternativa: ["Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.", 
+        alternativas: [
+            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.", 
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."],
     },
     {
         enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
-        alternativa: ["Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas", 
+        alternativa: [
+            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas", 
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."],
     },
     {
         enunciado: "Ao final da discussão, Gabriel precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativa:["Criar uma imagem utilizando um gerador de imagem de IA.", 
+        alternativas:[
+            "Criar uma imagem utilizando um gerador de imagem de IA.", 
             "Criar uma imagem utilizando uma plataforma de design como o Paint."],
-    }
+    },
 ];
 let atual = 0;
 let perguntaAtual;
+
 function mostraPergunta(){
     perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
@@ -41,6 +46,7 @@ function mostraAlternativas(){
         }
 }
 mostraPergunta();
+
 const lapis = {
   tamanho: 20,
   tipo: 'HB',
