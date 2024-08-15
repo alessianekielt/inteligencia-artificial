c35d50f691c2de7a7efc52b06ba579b1f67b626c
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-08-15 09:03:04 -0300

    1508

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ const perguntas = [
             {
                 texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
                 afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
+            },
         ]
     },
 ];
